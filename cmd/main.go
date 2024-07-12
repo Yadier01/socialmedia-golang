a2a203dfef4fefa8466cbaed9ee0853542d87038
yadier <yadierg7@gmail.com>
2024-07-12 19:40:49 -0400

    removed test struct (i was paying with vim)

--- cmd/main.go
+++ cmd/main.go
@@ -10,12 +10,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-}
-
 func main() {
 
 	config, err := util.LoadConfig("./")
